5f3dbc96f469582494a8a578b44affe510797d56
YE GAO <sortsimilar@gmail.com>
2021-01-16 16:45:57 +0800

    readlines

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -6,5 +6,25 @@ from trainer import Trainer
 from tester import Tester
 from params import Params
 
+dataset = Dataset('icews14')
+
+print(len(dataset.data["train"]))
+print(len(dataset.data["valid"]))
+print(len(dataset.data["test"]))
+
+ds_name = "icews14"
+ds_path = "datasets/" + ds_name.lower() + "/"
+filename = ds_path + "train.txt"
+
+with open(filename, "r", encoding='UTF-8') as f:
+    data = f.readlines()
+
+print(len(data))
+print(data[0])
+print(data[1])
+
+
+
+
 
 
