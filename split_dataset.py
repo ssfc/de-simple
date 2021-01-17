111dedd8ec5f3469a9367afb708c579540f0ad17
YE GAO <sortsimilar@gmail.com>
2021-01-17 20:41:46 +0800

    Update split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -19,22 +19,22 @@ ds_path = "datasets/" + ds_name.lower() + "/"
 filename = ds_path + "train.txt"  # this number of data in icews14 is 72826;
 
 with open(filename, "r", encoding='UTF-8') as f:
-    originalData = f.readlines()  # data in original dataset;
+    originalTrainData = f.readlines()  # data in original dataset;
 
 print(filename)
-print(len(originalData))
-print(originalData[0])
-print(originalData[len(originalData)-1])
+print(len(originalTrainData))
+print(originalTrainData[0])
+print(originalTrainData[len(originalTrainData)-1])
 
 if not os.path.exists('datasets/split_icews14'):
     os.makedirs('datasets/split_icews14')
 
 writeFileName = 'datasets/split_icews14/' + 'train.txt'
 
-numTrainTriple = int(len(originalData)/2)
+numTrainTriple = int(len(originalTrainData)/2)
 with open(writeFileName, 'w', encoding='UTF-8') as f:
     for i in range(numTrainTriple):
-        f.write("%s" % originalData[i])
+        f.write("%s" % originalTrainData[i])
 
 with open(writeFileName, "r", encoding='UTF-8') as f:
     data = f.readlines()
